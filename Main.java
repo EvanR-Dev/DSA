96f728f1537758ec5a274bdebf4a207e1597e073
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-05-30 21:04:34 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    DynamicArray<Integer> arr = new DynamicArray<>(8);
+    //DynamicArray<Integer> arr = new DynamicArray<>(8);
+    //Exponentiation exp = new Exponentiation();
+		//System.out.println(exp.logRecPow(2, 10));
   }
 }
