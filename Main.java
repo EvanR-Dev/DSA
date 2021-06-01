70d9a2dbfd50437d30a3ddd6f947ea7e86c38cb4
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-05-31 22:30:05 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public class Main {
     //DynamicArray<Integer> arr = new DynamicArray<>(8);
     //Exponentiation exp = new Exponentiation();
     //System.out.println(exp.logRecPow(2, 10));
+    Merge m = new Merge();
+		int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
+		m.mergeSort(arr, 0, arr.length - 1);
+		
+		for (int i : arr) System.out.println(i);
   }
 }
