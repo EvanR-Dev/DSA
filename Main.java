298f6d51764427c6657d9d8b82e7faa4dc71024b
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-05-31 22:30:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,9 +4,9 @@ public class Main {
     //Exponentiation exp = new Exponentiation();
     //System.out.println(exp.logRecPow(2, 10));
     Merge m = new Merge();
-		int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
-		m.mergeSort(arr, 0, arr.length - 1);
-		
-		for (int i : arr) System.out.println(i);
+    int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
+    m.mergeSort(arr, 0, arr.length - 1);
+	
+    for (int i : arr) System.out.println(i);
   }
 }
