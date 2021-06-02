27a083c25ff4d0a66fc4224eab56cbeacfe953c9
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-06-01 23:17:10 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,17 @@ public class Main {
     //DynamicArray<Integer> arr = new DynamicArray<>(8);
     //Exponentiation exp = new Exponentiation();
     //System.out.println(exp.logRecPow(2, 10));
-    Merge m = new Merge();
-    int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
-    m.mergeSort(arr, 0, arr.length - 1);
+    //Merge m = new Merge();
+    //int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
+    //m.mergeSort(arr, 0, arr.length - 1);
 	
-    for (int i : arr) System.out.println(i);
+    //for (int i : arr) System.out.println(i);
+    SlidingWindow sw = new SlidingWindow();
+    int x = sw.findMaxSumSubarray(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3);
+    System.out.println(x);
+    int y = sw.smallestSubarray(new int[] {4, 2, 2, 7, 8, 1, 2, 8, 10}, 8);
+    System.out.println(y);
+    int z = sw.findLength("AAAHHIBC", 2);
+    System.out.println(z);
   }
 }
