b819b4b4b03590da10def2508821a98acac9c63b
Evan <66287338+EvanR-Dev@users.noreply.github.com>
2021-06-02 23:45:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,16 +4,11 @@ public class Main {
     //Exponentiation exp = new Exponentiation();
     //System.out.println(exp.logRecPow(2, 10));
     //Merge m = new Merge();
-    //int[] arr = {9, 3, 7, 5, 6, 4, 8, 2};
-    //m.mergeSort(arr, 0, arr.length - 1);
-	
-    //for (int i : arr) System.out.println(i);
-    SlidingWindow sw = new SlidingWindow();
-    int x = sw.findMaxSumSubarray(new int[] {4, 2, 1, 7, 8, 1, 2, 8, 1, 0}, 3);
-    System.out.println(x);
-    int y = sw.smallestSubarray(new int[] {4, 2, 2, 7, 8, 1, 2, 8, 10}, 8);
-    System.out.println(y);
-    int z = sw.findLength("AAAHHIBC", 2);
-    System.out.println(z);
+    //SlidingWindow sw = new SlidingWindow();
+    TwoPointers tp = new TwoPointers();
+    int[] x = tp.threeSumSorted(new int[] {1, 2, 4, 5, 12}, 19);
+    for (int i : x) System.out.println(i);
+    int[] y = tp.sqEach(new int[] {-4, -3, 1, 2, 3});
+    for (int i : y) System.out.println(i);
   }
 }
